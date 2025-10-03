e69c745377301445eb95d0a7484185e90004091d
Miyuru Amarasiri <miyubhashi2002@gmail.com>
2025-10-03 14:03:24 +0530

    chore: update dependencies and add image optimization
    - Added image optimization dependencies: imagemin, image-minimizer-webpack-plugin, imagemin-mozjpeg, and imagemin-svgo to package.json.
    - Configured ImageMinimizerPlugin in webpack.config.prod.js for production builds to optimize images using specified plugins.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CopyPlugin = require('copy-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,6 +11,21 @@ module.exports = merge(common, {
       chunks: 'all',
     },
     runtimeChunk: 'single',
+    minimize: true,
+    minimizer: [
+      '...',
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['mozjpeg', { quality: 70, progressive: true }],
+              ['svgo', {}],
+            ],
+          },
+        },
+      }),
+    ],
   },
   plugins: [
     new CopyPlugin({
@@ -17,8 +33,8 @@ module.exports = merge(common, {
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
-  { from: 'CNAME', to: '.' },
-    { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'CNAME', to: '.' },
+        { from: 'icon.svg', to: 'icon.svg' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
